a6f11015b69ca45d4d9fbf4698328d866a9a2cae
User <User@WIN-8BH8LKBRQNO>
2023-12-21 20:41:39 +0500

    Customer

---
+++ BankApi.Repository/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApi.Repository
+{
+    public class Customer
+    {
+        public string? Name { get; set; }
+        public decimal CusAccountNum { get; set; }
+    }
+}
