90923a6cd9389f5163d758bbfefc2985c3b4d738
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-12-21 16:04:59 +0500

    DATA: Create BankAplication

---
+++ BankApi.Repository/BankAplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApi.Repository
+{
+    public class BankAplication
+    {
+        public decimal AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public void MakeDeposit()
+        {
+
+        }
+        public void WithdrawMoney()
+        {
+
+        }
+
+        public decimal GetBalance() =>  this.Balance;
+    }
+}
